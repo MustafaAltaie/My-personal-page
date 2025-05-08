7e982c201fb9acd95d96e4644b73988d8471123b
mustafa altaie <mustafaphoto111@gmail.com>
2025-05-08 12:10:29 +0200

    added dnd to educations

--- frontend/src/components/Educations.jsx
+++ frontend/src/components/Educations.jsx
@@ -1,7 +1,41 @@
+import { useState } from 'react';
 import '../styles/education.css';
 import { motion } from 'framer-motion';
 
 const Educations = ({ darkMode }) => {
+    const [list, setList] = useState([
+        {
+            title: 'Fullstack - JavaScript | Yrkeshögskoleutbildning',
+            date: '2023-09 - 2025-06 | Chas Academy, Linköping',
+            content: 'Jag studerade Fullstack JavaScript-utveckling på distans vid Chas Academy. Utbildningen var 100%. Studierna bedrevs både på svenska och engelska.'
+        },
+        {
+            title: '4-årig kandidatexamen inom IT',
+            date: '2012-09 - 2015-12 | Al-Rafidain universitet, Irak',
+            content: 'Jag studerade 4 år på fakulteten för datateknik. Utbildningen är validerad och godkänd i Sverige av UHR.'
+        }
+    ]);
+    const [draggedIndex, setDraggedIndex] = useState(null);
+
+    const handleDragStart = (index) => {
+        setDraggedIndex(index);
+    }
+
+    const handleDragOver = (event, index) => {
+        event.preventDefault();
+        if (draggedIndex === null || draggedIndex === index) return;
+        const newList = [...list];
+        const draggedItem = newList[draggedIndex];
+        newList.splice(draggedIndex, 1);
+        newList.splice(index, 0, draggedItem);
+        setDraggedIndex(index);
+        setList(newList);
+    }
+
+    const handleDrop = () => {
+        setDraggedIndex(null);
+    }
+
     return (
         <section className="educationSection">
             <div className='educationMainWrapper'>
@@ -13,28 +47,24 @@ const Educations = ({ darkMode }) => {
                     viewport={{ once: true, amount: 0.4 }}
                 ><i className="fa-solid fa-user-graduate"></i>Utbildningar</motion.h1>
                 <br /><br />
+                {list.map((education, index) => 
                 <motion.div
+                    key={index}
                     className='educationWrapper'
                     initial={{ opacity: 0, x: -150 }}
                     whileInView={{ opacity: 1, x: 0 }}
                     transition={{ duration: 0.7 }}
                     viewport={{ once: true, amount: 0.4 }}
+                    draggable
+                    onDragStart={() => handleDragStart(index)}
+                    onDragOver={e => handleDragOver(e, index)}
+                    onDrop={handleDrop}
                 >
-                    <h3 className='educationText'>Fullstack - JavaScript | Yrkeshögskoleutbildning</h3>
-                    <h5 className='educationText5'>2023-09 - 2025-06 | Chas Academy, Linköping</h5>
-                    <p className='educationText'>Jag studerade Fullstack JavaScript-utveckling på distans vid Chas Academy. Utbildningen var 100%. Studierna bedrevs både på svenska och engelska.</p>
+                    <h3 className='educationText'>{education.title}</h3>
+                    <h5 className='educationText5'>{education.date}</h5>
+                    <p className='educationText'>{education.content}</p>
                 </motion.div>
-                <motion.div
-                    className='educationWrapper'
-                    initial={{ opacity: 0, x: 150 }}
-                    whileInView={{ opacity: 1, x: 0 }}
-                    transition={{ duration: 0.7 }}
-                    viewport={{ once: true, amount: 0.4 }}
-                >
-                    <h3 className='educationText'>4-årig kandidatexamen inom IT</h3>
-                    <h5 className='educationText5'>2012-09 - 2015-12 | Al-Rafidain universitet, Irak</h5>
-                    <p className='educationText'>Jag studerade 4 år på fakulteten för datateknik. Utbildningen är validerad och godkänd i Sverige av UHR.</p>
-                </motion.div>
+                )}
             </div>
             <div className="educationIconWrapper flexMiddle">
                 <img src={darkMode ? "../../public/images/educationDark.png" : "../../public/images/educationLight.png"} alt="Education" />
